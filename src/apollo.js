a0bb28186fa6b735d1247bace2819f8a5ae5fda3
KeerthanaKS21 <keerthanaks.cse2024@citchennai.net>
2025-12-31 12:40:40 +0530

    modified url

--- src/apollo.js
+++ src/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://192.168.137.227:4000/graphql', // your backend
+  uri: process.env.REACT_APP_API_URL, // your backend
   }),
   cache: new InMemoryCache(),
 });
